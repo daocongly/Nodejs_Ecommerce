d24cd455430348a1f76e91284d73dc508f4084e7
Cong ly <lydcfx12998@funix.edu.vn>
2023-10-10 21:42:25 +0700

    express

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    console.log('baongoc')
+})
+
+app.use((req, res, next) => {
+    res.send('<h1>Hello world!</h1>');
+})
+
+
+app.listen(PORT, () => console.log("Server is running on port 3000"));
