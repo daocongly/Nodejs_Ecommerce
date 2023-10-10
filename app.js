449e9d0ac022d70a9cfe6140172246508cf70543
Cong ly <lydcfx12998@funix.edu.vn>
2023-10-10 22:01:59 +0700

    mongoose

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-    console.log('baongoc')
-    next();
-})
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    res.send('<h1>Hello world!</h1>');
-})
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.listen(PORT, () => console.log("Server is running on port 3000"));
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
